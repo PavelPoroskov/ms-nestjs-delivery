b8fbe41461fac3c511abcb654c8fd2cab5d07dc2
PavelPoroskov <pavel.poroskov.se@gmail.com>
2026-04-03 22:37:57 +0300

    connect logging service to mongodb

--- uber-services/apps/loggin-service/src/loggin-service.module.ts
+++ uber-services/apps/loggin-service/src/loggin-service.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LogginServiceController } from './loggin-service.controller';
 import { LogginServiceService } from './loggin-service.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+// TODO get mongo_connection_string from env
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://frybender:lila27zolberg37uncle@127.0.0.1:27017/logs_db',
+    ),
+  ],
   controllers: [LogginServiceController],
   providers: [LogginServiceService],
 })
