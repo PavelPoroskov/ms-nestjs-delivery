5ffa91c65469ec3528112b38bc3f508672e73283
PavelPoroskov <pavel.poroskov.se@gmail.com>
2026-04-03 20:21:05 +0300

    set different port for every service

--- uber-services/apps/loggin-service/src/main.ts
+++ uber-services/apps/loggin-service/src/main.ts
@@ -3,6 +3,6 @@ import { LogginServiceModule } from './loggin-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LogginServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3002);
 }
 bootstrap();
