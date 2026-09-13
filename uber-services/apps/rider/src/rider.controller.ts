918530cff8f43b75f2df6ae29c891ec6e986b36d
PavelPoroskov <pavel.poroskov.se@gmail.com>
2026-09-13 11:26:07 +0300

    add inter microservices communication. unfinished

--- uber-services/apps/rider/src/rider.controller.ts
+++ uber-services/apps/rider/src/rider.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { RiderService } from './rider.service';
 
-@Controller()
+@Controller('rider')
 export class RiderController {
   constructor(private readonly riderService: RiderService) {}
 
-  @Get()
-  getHello(): string {
-    return this.riderService.getHello();
+  @Get(':id')
+  getRiderById(
+    @Param()
+    params: any
+  ) {
+    return {
+      _id: params.id,
+      firstName: 'Joe',
+      lastName: 'Smith',
+      email: 'joe@gmail.com',
+    };
   }
 }
