d8bc71acbb2953112ab596fb023219b4f7745c5d
PavelPoroskov <pavel.poroskov.se@gmail.com>
2026-09-13 12:08:03 +0300

    nest generate module rider-coordinates

--- uber-services/apps/logging/src/logging.module.ts
+++ uber-services/apps/logging/src/logging.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoggingController } from './logging.controller.js';
 import { LoggingService } from './logging.service.js';
+import { RiderCoordinatesModule } from './rider-coordinates/rider-coordinates.module';
 
 @Module({
-  imports: [],
+  imports: [RiderCoordinatesModule],
   controllers: [LoggingController],
   providers: [LoggingService],
 })
